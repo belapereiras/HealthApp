67e9a24c27704f503ac89ec118cd14d0fa34c7a7
gpmarques <guilhermepmarques@globo.com>
2016-07-25 15:36:52 -0300

    Plist

--- DoNotSmokeApp/AppDelegate.swift
+++ DoNotSmokeApp/AppDelegate.swift
@@ -16,6 +16,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+//        let plist = Plist(name: "UserPropertyList")!
+//        let userDic = plist.getValuesInPlistFile()!
+//        if (userDic["FirstTime"] != nil) {
+//            let firstTime = userDic["FirstTime"] as! Bool
+//            if !firstTime {
+//                let storyboard = UIStoryboard(name: "Main", bundle: nil)
+//                let vc = storyboard.instantiateViewControllerWithIdentifier("ProgressScene")
+//                self.window?.rootViewController = vc
+//            }
+//        } else {
+//            print("NIL VALUE")
+//        }
         return true
     }
 
@@ -40,22 +52,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-    
-    func applicationDidFinishLaunching(application: UIApplication) {
-//        guard let plist = Plist(name: "UserPropertyList") else { return }
-//        guard let userDic = plist.getValuesInPlistFile() else { return }
-//        if (userDic["FirstTime"] != nil) {
-//            let firstTime = userDic["FirstTime"] as! Bool
-//            if !firstTime {
-//                let storyboard = UIStoryboard(name: "Main", bundle: nil)
-//                let vc = storyboard.instantiateViewControllerWithIdentifier("") as! UIViewController
-//                self.window?.rootViewController = vc
-//            }
-//        } else {
-//            print("NIL VALUE")
-//        }
-    }
-
 
 }
 
